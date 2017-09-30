564b6b202f4c316e9aff0f831ced1b5bc42284d5
mradol <tuanbk189@gmail.com>
2017-09-30 22:54:52 +1000

    Fix Template Store, Fix myContact

--- my-contacts/app/app.js
+++ my-contacts/app/app.js
@@ -3,6 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('myContacts', [
   'ngRoute',
+  'firebase',
+  'myContacts.contacts'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
