d75e9d8cfbb72ec62d8372f3db95284be9cdcf81
mradol <tuanbk189@gmail.com>
2017-11-15 10:39:31 +1100

    Watchers and the Digest Loop, $apply, $timeout

--- udemy/app.js
+++ udemy/app.js
@@ -2,12 +2,30 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope','$filter', function ($scope, $filter) {
+angularApp.controller('mainController', ['$scope', '$filter', '$timeout', function ($scope, $filter, $timeout) {
 
   $scope.handle = '';
 
-  $scope.lowercasehandle = function() {
+  $scope.lowercasehandle = function () {
     return $filter('lowercase')($scope.handle);
   }
 
+  $scope.$watch('handle', function (newValue, oldValue) {
+    console.info('Changed!');
+    console.log('Old:' + oldValue);
+    console.log('New:' + newValue)
+
+  })
+
+  // setTimeout(function() {
+  //   $scope.$apply(function() {
+  //     $scope.handle = 'newtwitterhandle';
+  //     console.log('Scope changed')
+  //   });
+  // }, 3000)
+
+  $timeout(function () {
+    $scope.handle = 'newtwitterhandle';
+    console.log('Scope changed')
+  }, 3000)
 }]);
