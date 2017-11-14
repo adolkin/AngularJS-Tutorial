aa3d6edbebdadd07292b940bb7c32b3dd39ea4ea
mradol <tuanbk189@gmail.com>
2017-11-14 23:47:52 +1100

    $scope, $log and $filter

--- udemy/app.js
+++ udemy/app.js
@@ -2,13 +2,16 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
-    
+angularApp.controller('mainController', ['$scope','$log','$filter', function ($scope, $log, $filter) {
+    // $log.log("Hello Log");
+    // $log.info("info");
+    // $log.warn("warn");
+    // $log.debug("debug");
+    // $log.error("error");
+
+    $scope.name = 'Jonh';
+    $scope.formattedname = $filter('uppercase')($scope.name);
+
+    $log.info($scope.name);
+    $log.info($scope.formattedname);
 }]);
-
-var searchPeople = function searchPeople(firstName, lastName, height, age, occupation) {
-  return 'Jane Doe';
-}
-
-console.log(angular.injector().annotate(searchPeople));
-
