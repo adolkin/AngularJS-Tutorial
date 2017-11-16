52da6b29991f9376ceffaa30cbd89b1297fbfc23
mradol <tuanbk189@gmail.com>
2017-11-16 14:27:22 +1100

    $routeParams

--- udemy/app.js
+++ udemy/app.js
@@ -1,22 +1,27 @@
 var myApp = angular.module("angularApp", ["ngRoute"]);
-myApp.config(function($routeProvider){
-    $routeProvider
-       .when("/", {
-        templateUrl: "pages/main.html",
-        controller: "mainController"
+myApp.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "pages/main.html",
+      controller: "mainController"
     })
     .when("/first", {
-        templateUrl:"pages/first.html",
-        controller: "firstController"
-    });
+      templateUrl: "pages/first.html",
+      controller: "firstController"
+    })
+    .when("/first/:num", {
+    templateUrl: "pages/first.html",
+    controller: "firstController"
+  });
 });
 
-myApp.controller("mainController", ["$scope","$log", function($scope, $log){
+myApp.controller("mainController", ["$scope", "$log", function ($scope, $log) {
   $scope.name = 'Main';
 }]);
 
-myApp.controller("firstController", ["$scope","$log", function($scope, $log){
-    $scope.name = 'First';
+myApp.controller("firstController", ["$scope", "$log", "$routeParams", function ($scope, $log, $routeParams) {
+  $scope.name = 'First';
+  $scope.num = $routeParams.num;
 }]);
 
 
