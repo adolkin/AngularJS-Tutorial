b841db0a689d018af4d394800b87b179d95c83a2
mradol <tuanbk189@gmail.com>
2017-11-18 10:41:58 +1100

    ng-repeat Directives

--- udemy/app.js
+++ udemy/app.js
@@ -10,40 +10,55 @@ myApp.config(function ($routeProvider) {
       controller: "firstController"
     })
     .when("/first/:num", {
-    templateUrl: "pages/first.html",
-    controller: "firstController"
-  });
+      templateUrl: "pages/first.html",
+      controller: "firstController"
+    });
 });
 
-myApp.service("nameService", function() {
+myApp.service("nameService", function () {
   var self = this;
-  this.name="John Doe";
+  this.name = "John Doe";
 
-  this.namelength = function() {
+  this.namelength = function () {
     return self.name.length;
   }
 
 })
 
 myApp.controller("mainController", ["$scope", "$log", "nameService", function ($scope, $log, nameService) {
-  $scope.person = {
-    name: 'John Doe',
-    address: '555 Main St.', 
-    city: 'New York',
-    state: 'NY',
-    zip: '1111'
-  }
-
-  $scope.formattedAddress = function(person) {
+  $scope.people = [
+    {
+      name: 'John Doe',
+      address: '555 Main St.',
+      city: 'New York',
+      state: 'NY',
+      zip: '1111'
+    },
+    {
+      name: 'Jane Doe',
+      address: '666 Main St.',
+      city: 'New York',
+      state: 'NY',
+      zip: '1111'
+    },
+    {
+      name: 'George Doe',
+      address: '777 Main St.',
+      city: 'New York',
+      state: 'NY',
+      zip: '1111'
+    }
+  ]
+  $scope.formattedAddress = function (person) {
     return person.address + ', ' + person.city + ', ' + person.state + ', ' + person.zip;
   }
 }]);
 
-myApp.controller("firstController", ["$scope", "$log", "$routeParams","nameService", function ($scope, $log, $routeParams, nameService) {
+myApp.controller("firstController", ["$scope", "$log", "$routeParams", "nameService", function ($scope, $log, $routeParams, nameService) {
 
 }]);
 
-myApp.directive("searchResult", function() {
+myApp.directive("searchResult", function () {
   return {
     restrict: 'AECM',
     templateUrl: 'directives/searchresult.html',
