fb65c773ee06b06a2609fdb846687350da07e9f3
mradol <tuanbk189@gmail.com>
2017-11-21 18:44:51 +1100

    Custom Service To Share Data

--- weather-forecast/app.js
+++ weather-forecast/app.js
@@ -16,11 +16,21 @@ weatherApp.config(function ($routeProvider) {
   })
 })
 
-// CONTROLERS
-weatherApp.controller('homeController', ['$scope', function ($scope) {
 
+// SERVICES
+weatherApp.service('cityService', function() {
+  this.city = "New York, NY"
+})
+
+
+// CONTROLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
+  $scope.city = cityService.city;
+  $scope.$watch('city', function() {
+    cityService.city = $scope.city;
+  })
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function ($scope) {
-
+weatherApp.controller('forecastController', ['$scope', 'cityService', function ($scope, cityService) {
+  $scope.city = cityService.city;
 }]);
