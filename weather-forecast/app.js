524f4f137af9c88df09c1c9c6ccc5d11852fc90a
mradol <tuanbk189@gmail.com>
2017-11-21 20:11:57 +1100

    Binding Data P2

--- weather-forecast/app.js
+++ weather-forecast/app.js
@@ -39,7 +39,7 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function ($sco
 weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService, ) {
   $scope.city = cityService.city;
   
-  $scope.days = $routeParams.days || 2;
+  $scope.days = $routeParams.days || '2';
 
   $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?APPID=22a06018ceec012c7883c1cad81833b0",
     { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" } });
