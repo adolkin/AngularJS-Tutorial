7a73fc59a0ae2b802e3aee48167054b2e6f3e348
mradol <tuanbk189@gmail.com>
2017-12-31 23:51:51 +1100

    Ui-router-tutorial

--- foursquare/app/controllers/placesExplorerController.js
+++ foursquare/app/controllers/placesExplorerController.js
@@ -1,7 +1,30 @@
 (function (){
   'use strict';
   var app = angular.module('FoursquareApp');
-  app.controller('placesExplorerController', function ($scope) {
+  app.controller('placesExplorerController', function ($scope, placesExplorerService, $filter) {
     $scope.exploreNearby = "New York";
+    $scope.exploreQuery = "";
+    $scope.filterValue = "";
+
+    $scope.paces = [];
+    $scope.filteredPlaces = [];
+    $scope.filteredPlacesCount = 0;
+
+    $scope.totalRecordsCount = 0;
+    $scope.pageSize = 10;
+    $scope.currentPage = 1;
+
+    init();
+
+    function init() {
+      createWatch();
+      getPlaces();
+    }
+
+    function getPlaces() {
+      var offset = ($scope.pageSize) * ($scope.currentPage - 1);
+      
+      placesExplorerService.get({ near: $scope.ex})
+    }
   })
 })()
